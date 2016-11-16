9cb96776b799cc383e405614b13714345151ca3e
stubbornnessness <1219721814yxj>
2016-11-16 23:10:59 +0800

    3.0.1

---
+++ SwiftString_iOS/SwiftString_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftString_iOS.h
+//  SwiftString_iOS
+//
+//  Created by yuanxiaojun on 2016/11/16.
+//  Copyright © 2016年 袁晓钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftString_iOS.
+FOUNDATION_EXPORT double SwiftString_iOSVersionNumber;
+
+//! Project version string for SwiftString_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftString_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftString_iOS/PublicHeader.h>
+
+
--- Tests/LinuxMain.swift
+++
@@ -1,5 +0,0 @@
-import XCTest
-import SwiftStringTestSuite
-var tests = [XCTestCaseEntry]()
-tests += SwiftStringTestSuite.allTests()
-XCTMain(tests)
